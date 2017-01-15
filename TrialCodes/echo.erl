bcfb4c6ae59e26b8932dbf320d50442aaf64cfd4
Mohit Reddy <mohitreddy1996@gmail.com>
2017-01-15 19:31:11 +0530

    .beam files removed

--- TrialCodes/echo.erl
+++ TrialCodes/echo.erl
@@ -2,7 +2,8 @@
 %%% @author mohit
 %%% @copyright (C) 2017, <COMPANY>
 %%% @doc
-%%%
+%%% Replication of echo call in unix. Send back whatever data is sent to
+%%% it over a TCP Connection.
 %%% @end
 %%% Created : 15. Jan 2017 7:18 PM
 %%%-------------------------------------------------------------------
@@ -10,4 +11,30 @@
 -author("mohit").
 
 %% API
--export([]).
+-export([listen/1]).
+
+% define a macro.
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+
+% listen(port) to start the service
+listen(Port) ->
+  {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+  accept(LSocket).
+
+% accept the incoming socket
+accept(LSocket) ->
+  {ok, Socket} = gen_tcp:accept(LSocket),
+  % spawn creates a new process for a functional object. Function can be used a fun object.
+  spawn(fun() -> loop(Socket) end),
+  accept(LSocket).
+
+% loop makes sure of sending back the same packet/data received.
+% replicates the functionality of echo.
+loop(Socket)  ->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, Data}  ->
+      gen_tcp:send(Socket, Data),
+      loop(Socket);
+    {error, closed} ->
+      ok
+  end.
