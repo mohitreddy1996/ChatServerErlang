9d0fa0df47871d65c2e5060075e64f036172922a
Vilas M <vilasnitk19@gmail.com>
2017-01-16 21:39:56 +0530

    echo2 modified

--- TrialCodes/echo2.erl
+++ TrialCodes/echo2.erl
@@ -4,27 +4,28 @@
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 listen(Port) ->
-    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    accept(LSocket).
+  Pid = spawn_link(fun() ->
+    {ok, Listen} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+    spawn(fun() -> accept(Listen) end),
+    timer:sleep(infinity)
+                   end),
+  {ok, Pid}.
 
 accept(LSocket) ->
-    {ok, Socket} = gen_tcp:accept(LSocket),
-    spawn(fun() -> loop(Socket) end),
-    accept(LSocket).
-
-%%get_msg(['h','e','l','l','o']) ->  "Hi";
-
-get_msg(_) ->  "Message Received".
+  {ok, Socket} = gen_tcp:accept(LSocket),
+  spawn(fun() -> accept(LSocket) end),
+  loop(Socket).
 
 loop(Socket) ->
-      case gen_tcp:recv(Socket, 0) of
-        {ok, Data} ->
-          Msg = get_msg(erlang:binary_to_list(Data)),
-          case gen_tcp:send(Socket, Msg) of
-              {error, _} -> exit(normal);
-               ok -> loop(Socket)
-          end;
-        {error, _} -> ok
-			end.
-	    	
+  inet:setopts(Socket, [{active, once}]),
+  receive
+    {tcp, Socket, <<"quit", _/binary>>} ->
+      gen_tcp:close(Socket);
+    {tcp, Socket, <<"hello", _/binary>>} ->
+      gen_tcp:send(Socket,"Hi! What can i do for you?\n"),
+      loop(Socket);
+    {tcp, Socket, Msg} ->
+      gen_tcp:send(Socket, Msg),
+      loop(Socket)
+  end.
 
